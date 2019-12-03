7a475a34610bcd07dadaeee55c27e9c147d60ad8
Stanley Haryoto <stanley.haryoto@gmail.com>
2019-12-03 13:02:18 +0800

    Moving to github

---
+++ client/src/components/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Header from './Header';
+
+const Dashboard = () => {
+    return (<h2>Dashboard</h2>);
+};
+
+const SurveyNew = () => {
+    return (<h2>Survey New</h2>);
+};
+
+const Landing = () => {
+    return (<h2>Landing</h2>);
+}
+
+const App = () => {
+    return (
+    <div>
+        <BrowserRouter>
+            <div className="container">
+                <Header />
+                <Route exact path="/" component={Landing} />
+                <Route exact path="/surveys" component={Dashboard} />
+                <Route exact path="/surveys/new" component={SurveyNew} />
+            </div>
+        </BrowserRouter>
+    </div>
+    );
+};
+
+export default App;
---
+++ client/src/components/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <nav>
+                <div className="nav-wrapper">
+                <a href="#" className="brand-logo">Emaily</a>
+                <ul id="nav-mobile" class="right">
+                    <li><a>Login with Google</a></li>
+                </ul>
+                </div>
+            </nav>
+        )
+    }
+}
+
+export default Header;
