3b6af93bfa3b8bcb8c967f5c499cc10cd7ee5ea0
Stanley Haryoto <stanley.haryoto@gmail.com>
2020-03-08 17:41:55 +0800

    Completed payment feature using Stripe

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 import Header from './Header';
+import Landing from './Landing';
 
 const Dashboard = () => {
     return (<h2>Dashboard</h2>);
@@ -11,23 +14,28 @@ const SurveyNew = () => {
     return (<h2>Survey New</h2>);
 };
 
-const Landing = () => {
-    return (<h2>Landing</h2>);
+
+class App extends Component {
+
+    componentDidMount() {
+        this.props.fetchUser();
+    }
+
+    render() {
+        return (
+            <div>
+                <BrowserRouter>
+                    <div className="container">
+                        <Header />
+                        <Route exact path="/" component={Landing} />
+                        <Route exact path="/surveys" component={Dashboard} />
+                        <Route exact path="/surveys/new" component={SurveyNew} />
+                    </div>
+                </BrowserRouter>
+            </div>
+            );
+    }
+
 }
 
-const App = () => {
-    return (
-    <div>
-        <BrowserRouter>
-            <div className="container">
-                <Header />
-                <Route exact path="/" component={Landing} />
-                <Route exact path="/surveys" component={Dashboard} />
-                <Route exact path="/surveys/new" component={SurveyNew} />
-            </div>
-        </BrowserRouter>
-    </div>
-    );
-};
-
-export default App;
+export default connect(null, actions) (App);
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Payments from './Payment';
 
 class Header extends Component {
+
+    renderContent() {
+        switch(this.props.auth) {
+            case null:
+                return;
+            
+            case false:
+                return (<li><a href="/auth/google">Login with Google</a></li>);
+
+            default:
+                return [
+                    <li key="1"><Payments /></li>,
+                    <li key="3" style={{ margin: '0 10px' }}>
+                        Credits: {this.props.auth.credits}
+                    </li>,
+                    <li key="2"><a href="/api/logout">Logout</a></li>
+                ];
+        }
+    }
+
+
     render() {
         return (
             <nav>
                 <div className="nav-wrapper">
-                <a href="#" className="brand-logo">Emaily</a>
-                <ul id="nav-mobile" class="right">
-                    <li><a>Login with Google</a></li>
+                <Link 
+                    to={this.props.auth ? '/surveys' : '/'} 
+                    className="brand-logo"
+                    >
+                        Emaily
+                </Link>
+                <ul id="nav-mobile" className="right">
+                    { this.renderContent() }
                 </ul>
                 </div>
             </nav>
@@ -15,4 +44,8 @@ class Header extends Component {
     }
 }
 
-export default Header;
+function mapToStateProps({ auth }) {
+    return { auth };
+}
+
+export default connect(mapToStateProps)(Header);
